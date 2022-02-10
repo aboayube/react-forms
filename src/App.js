4a9beded6877c9d654438b9ca496bcf503c7baf5
unknown <snuora2019@gmail.com>
2022-02-10 09:42:33 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import FormThree from './pages/FormThree/FormThree';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FormThree />
+    </div>
+  );
+}
+
+export default App;
